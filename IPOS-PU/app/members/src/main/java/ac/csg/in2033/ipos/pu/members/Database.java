ff79d99c64173878573c17b6db99d08b67bef296
Asali Khokhar <Furqan.Khokhar@city.ac.uk>
2026-04-05 21:28:31 +0100

    feat: organise user database class connection
    This commit comments out database connection code and places user.db database into data file to separate from other files.

--- IPOS-PU/app/members/src/main/java/ac/csg/in2033/ipos/pu/members/Database.java
+++ IPOS-PU/app/members/src/main/java/ac/csg/in2033/ipos/pu/members/Database.java
@@ -12,14 +12,32 @@ import java.sql.DriverManager;
 public class Database {
     private final static Logger logger = LoggerFactory.getLogger(Database.class);
 
+    private static final String DB_DIR = System.getProperty("user.dir") + "/data";
     private static final String DB_NAME = "users.db";
 
     public static Connection connect() {
         try {
-            File dbFile = new File(DB_NAME);
+            // Define data directory
+            File dir = new File(DB_DIR);
+            // If directory does not exist
+            if (!dir.exists()) {
+                // Create data directory
+                // If data directory cannot be created
+                if (!dir.mkdirs()) {
+                    // Log failure to create directory
+                    logger.error("Failed to create database directory: {}", dir.getAbsolutePath());
+                }
+            }
+
+            // Create user database file
+            File dbFile = new File(dir, DB_NAME);
+
+            // FIXME: Temporary debugging printing
             System.out.println("DB path: " + dbFile.getAbsolutePath());
             System.out.println("DB exists: " + dbFile.exists());
 
+            // If database doesn't exist and test db does exist
+            // Copy test data to real database
             if (!dbFile.exists()) {
                 InputStream is = Database.class.getResourceAsStream("/sql/" + DB_NAME);
                 if (is != null) {
@@ -27,6 +45,7 @@ public class Database {
                 }
             }
 
+            // Return connection to database
             String url = "jdbc:sqlite:" + dbFile.getAbsolutePath();
             return DriverManager.getConnection(url);
 
